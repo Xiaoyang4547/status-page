156f77442802877f312794f987146304aa71e706
Xiaoyang4547 <ad3202655736@outlook.com>
2025-08-02 18:00:38 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -65,14 +65,14 @@ const workerConfig: WorkerConfig = {
     },
     // Example TCP Monitor
     {
-      id: 'foo_monitor',
-      name: '必应',
+      id: 'no_out',
+      name: '订阅提醒',
       // `method` should be `TCP_PING` for tcp monitors
       method: 'POST',
       // `target` should be `host:port` for tcp monitors
-      target: 'https://cn.bing.com‘,
+      target: 'https://time.xiaoyang4547.dpdns.org',
       tooltip: 'My production server SSH',
-      statusPageLink: 'https://cn.bing.com',
+      statusPageLink: 'https://time.xiaoyang4547.dpdns.org',
       timeout: 10000,
     },
     {
