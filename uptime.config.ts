633ae87e8be9c9efc849a68211abd1ec5aa3ce5d
Xiaoyang4547 <ad3202655736@outlook.com>
2025-08-03 10:12:04 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -32,9 +32,9 @@ const workerConfig: WorkerConfig = {
       // `name` is used at status page and callback message
       name: '个人网站',
       // `method` should be a valid HTTP Method
-      method: 'OPTIONS',
+      method: 'GET https://xiaoyang4547.dpdns.org/about.html',
       // `target` is a valid URL
-      target: 'https://xiaoyang4547.dpdns.org/',
+      target: '',
       // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
       tooltip: 'This is a tooltip for this monitor',
       // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
